4201bce14f9c25f2a7bdb197c024858dff9e9ee7
Yernar <168364849+theYernar@users.noreply.github.com>
2024-09-24 15:18:12 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+// Открытие модального окна для выбора товара
 function openModal() {
     document.getElementById('modal').style.display = 'flex';
 }
 
+// Закрытие модального окна выбора товара
 function closeModal() {
     document.getElementById('modal').style.display = 'none';
+}
+
+// Открытие модального окна для добавления товара
+function openAddProductModal() {
+    document.getElementById('add-product-modal').style.display = 'flex';
+}
+
+// Закрытие модального окна добавления товара
+function closeAddProductModal() {
+    document.getElementById('add-product-modal').style.display = 'none';
+}
+
+// Закрытие модальных окон при клике вне их содержимого
+window.onclick = function(event) {
+    if (event.target === document.getElementById('modal')) {
+        closeModal();
+    } else if (event.target === document.getElementById('add-product-modal')) {
+        closeAddProductModal();
+    }
 }
